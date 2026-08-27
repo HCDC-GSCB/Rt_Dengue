42148e7a2679b6470fc02ec43b62bcb2850f097a
Mongemto1399 <truongkequan2@gmail.com>
2026-08-27 11:41:28 +0700

    Update directory for EpiFilter files_2

--- setup.R
+++ setup.R
@@ -10,7 +10,7 @@ library(coga)
 library(EpiEstim)
 
 # Gọi các functions của EpiFilter:
-path_epifilter <- "package/EpiFilter/R files"
+path_epifilter <- "package/EpiFilter"
 
 files.sources <- list.files(path = path_epifilter, full.names = TRUE)
   for (f in files.sources) {
